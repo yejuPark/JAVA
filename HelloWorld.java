8d6ac875867b7cb48bed4b2b977540fbf44abada
yejuPark <pyj8977@daum.net>
2024-03-08 17:03:47 +0900

    part1_end

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,16 @@
 public class HelloWorld {
-// main : 프로그램의 시작점
+  // 프로그램의 시작점
+  // compiler : 작성한 코드를 실행가능한 파일로 변환
+  /**
+   * 
+   */
   public static void main(String[] args){
     System.out.println("Hello World");
+    // System.out.println("test");
+    /* System.out.println("1");
+    System.out.println("2");
+    System.out.println("3"); */
+    System.out.println("안녕");
   }
 }
+
